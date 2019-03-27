6e63d8c80319f231ff0d128eed10276a92e252f3
Yuan Gao <vincentg@unity3d.com>
2019-03-27 14:56:21 -0700

    Added the cloud build script

---
+++ UnitySDK/Assets/ML-Agents/Scripts/CloudBuild/Builder.cs
@@ -0,0 +1,33 @@
+﻿//#if UNITY_CLOUD_BUILD
+
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace MLAgents.CloudBuild
+{
+    public class Builder
+    {
+        public static void PreExport()
+        {
+            var aca = SceneAsset.FindObjectOfType<Academy>();
+		
+			var learningBrains = aca.broadcastHub.broadcastingBrains.Where(
+				x => x != null && x is LearningBrain);
+			
+			foreach (Brain brain in learningBrains)
+			{
+				if (!aca.broadcastHub.IsControlled(brain))
+				{
+					aca.broadcastHub._brainsToControl.Add(brain);
+					Debug.Log("Switched brain " + brain.name + " to Control Mode");
+				}
+			}
+	        
+	        Debug.Log("The current scene " + EditorSceneManager.GetActiveScene().name + " has switched its all of its brain to Control Mode");
+        }
+    }
+}
+
+//#endif
