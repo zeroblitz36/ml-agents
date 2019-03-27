c55337cb955cfecef57c4dd0a132e6b541bc1b14
Yuan Gao <vincentg@unity3d.com>
2019-03-27 16:52:43 -0700

    Changed the method to empty to debug

--- UnitySDK/Assets/Editor/Builder.cs
+++ UnitySDK/Assets/Editor/Builder.cs
@@ -11,21 +11,7 @@ namespace MLAgents
     {
         public static void PreExport()
         {
-            var aca = SceneAsset.FindObjectOfType<Academy>();
-		
-			var learningBrains = aca.broadcastHub.broadcastingBrains.Where(
-				x => x != null && x is LearningBrain);
-			
-			foreach (Brain brain in learningBrains)
-			{
-				if (!aca.broadcastHub.IsControlled(brain))
-				{
-					aca.broadcastHub._brainsToControl.Add(brain);
-					Debug.Log("Switched brain " + brain.name + " to Control Mode");
-				}
-			}
-	        
-	        Debug.Log("The current scene " + EditorSceneManager.GetActiveScene().name + " has switched its all of its brain to Control Mode");
+
         }
     }
 }
