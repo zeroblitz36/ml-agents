3c1a7347373dae49934af7352ea1e60ed6995908
Yuan Gao <vincentg@unity3d.com>
2019-03-27 17:27:18 -0700

    Removed the ML-Agents namespace

--- UnitySDK/Assets/Editor/Builder.cs
+++ UnitySDK/Assets/Editor/Builder.cs
@@ -5,26 +5,24 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
-namespace MLAgents
+public class Builder
 {
-    public static class Builder
-    {
-        public static void PreExport()
-        {
-            var aca = SceneAsset.FindObjectOfType<Academy>();
+	public static void PreExport()
+	{
+		var aca = SceneAsset.FindObjectOfType<Academy>();
+	
+		var learningBrains = aca.broadcastHub.broadcastingBrains.Where(
+			x => x != null && x is LearningBrain);
 		
-			var learningBrains = aca.broadcastHub.broadcastingBrains.Where(
-				x => x != null && x is LearningBrain);
-			
-			foreach (Brain brain in learningBrains)
+		foreach (Brain brain in learningBrains)
+		{
+			if (!aca.broadcastHub.IsControlled(brain))
 			{
-				if (!aca.broadcastHub.IsControlled(brain))
-				{
-					aca.broadcastHub._brainsToControl.Add(brain);
-				}
+				aca.broadcastHub._brainsToControl.Add(brain);
 			}
-        }
-    }
+		}
+	}
 }
+
 
 #endif
